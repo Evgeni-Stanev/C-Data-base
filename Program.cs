76c73a92609eacc108c0124f6252bf2ca0d488aa
Evgeni-Stanev <124701905+Evgeni-Stanev@users.noreply.github.com>
2023-02-12 19:15:18 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = int.Parse(Console.ReadLine());
+double b = a / 1000.00;
+Console.WriteLine($"{b:f2}");
