d7a92fdd24fad1410faac4f88fb241cee58a1bf3
Evgeni-Stanev <124701905+Evgeni-Stanev@users.noreply.github.com>
2023-02-18 13:07:40 +0200

    Create Zad 8 Arrays.cs

---
+++ Zad 8 Arrays.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sums = new int[nums.Length];
+
+
+
+            for (int j = 0; j < nums.Length-1; j++)
+            {
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    nums[i] = nums[i] + nums[i + 1];
+                }
+                nums[nums.Length - j-1] = 0;
+            }
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.Write(nums[0]);
+                }
+            
+
+
+            
+        }
+    }
+}
